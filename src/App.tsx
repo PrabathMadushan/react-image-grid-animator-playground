3c6295500d3a90c3ba71e464700deec226170229
prabathmadushan5@gmail.com <prabathmadushan5@gmail.com>
2021-01-09 00:26:40 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "react-slideshow-image/dist/styles.css";
+import image1 from "./1.jpg";
+import image2 from "./2.jpg";
+import image3 from "./3.jpg";
+import { Fade } from "react-slideshow-image";
 
 function App() {
+  const fadeImages = [image1, image2, image3];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="brand-slider">
+        <div style={{ width: "700px", height: "600px" }}>
+          <h2>Fade Effect</h2>
+          <div className="slide-container">
+            <Fade duration={1000} infinite indicators={false} arrows={false}>
+              <div className="each-fade">
+                <div>
+                  <img alt="" src={fadeImages[0]} />
+                </div>
+              </div>
+              <div className="each-fade">
+                <div>
+                  <img alt="" src={fadeImages[1]} />
+                </div>
+              </div>
+              <div className="each-fade">
+                <div>
+                  <img alt="" src={fadeImages[2]} />
+                </div>
+              </div>
+            </Fade>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
