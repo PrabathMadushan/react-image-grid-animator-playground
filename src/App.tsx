248c2f35c035b5ce2539b892d4a069be400c1469
prabathmadushan5@gmail.com <prabathmadushan5@gmail.com>
2021-01-09 00:36:28 +0530

    push

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,32 @@ import image3 from "./3.jpg";
 import { Fade } from "react-slideshow-image";
 
 function App() {
-  const fadeImages = [image1, image2, image3];
   return (
     <div className="App">
       <div className="brand-slider">
         <div style={{ width: "700px", height: "600px" }}>
           <h2>Fade Effect</h2>
           <div className="slide-container">
-            <Fade duration={1000} infinite indicators={false} arrows={false}>
+            <Fade
+              duration={2000}
+              infinite
+              indicators={false}
+              arrows={false}
+              transitionDuration={1000}
+            >
               <div className="each-fade">
                 <div>
-                  <img alt="" src={fadeImages[0]} />
+                  <img alt="" src={image1} />
                 </div>
               </div>
               <div className="each-fade">
                 <div>
-                  <img alt="" src={fadeImages[1]} />
+                  <img alt="" src={image2} />
                 </div>
               </div>
               <div className="each-fade">
                 <div>
-                  <img alt="" src={fadeImages[2]} />
+                  <img alt="" src={image3} />
                 </div>
               </div>
             </Fade>
